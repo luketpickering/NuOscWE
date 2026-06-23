c22fe0b8d9a8c51c046c338a60c75ff8c5c55a14
Luke Pickering <luketpickering@gmail.com>
2026-06-23 23:54:19 +0000

    improves osc prob playground

--- proj_utils/__init__.py
+++ proj_utils/__init__.py
@@ -309,31 +309,89 @@ def Probability_Matter_LBL(E, osc_params, osc_channels=[]):
 
 from matplotlib.widgets import Slider
 
-def InteractiveOscProbPlot(Es, osc_params):
+InteractiveOscProbPlot_fig = None
+def InteractiveOscProbPlot(osc_params):
+  global InteractiveOscProbPlot_fig
+  if InteractiveOscProbPlot_fig is not None:
+    plt.close(InteractiveOscProbPlot_fig)
+
+  colors = {
+    "numu": "#AA3377",
+    "nue": "#4477AA",
+    "antinumu": "#EE6677",
+    "antinue": "#66CCEE",
+  }
+  
+  Energy_Distribution_GeV = osc_params["experimental_energy_distribution"]
+  
+  Min_E_GeV = Energy_Distribution_GeV["peak"] - 3 * Energy_Distribution_GeV["width"]
+  Min_E_GeV = Min_E_GeV if Min_E_GeV > 0 else Energy_Distribution_GeV["peak"]/5
+  Max_E_GeV = Energy_Distribution_GeV["peak"] + 5 * Energy_Distribution_GeV["width"]
+
+  # Es = np.logspace(np.log(Peak_E_GeV/2),np.log(Peak_E_GeV*2),1000)
+  Es = np.linspace(Min_E_GeV, Max_E_GeV,1000)
+  
   var_params = osc_params.copy()
   
   numu_surv_prob, antinumu_surv_prob, nue_app_prob, antinue_app_prob = \
     Probability_Matter_LBL(Es, var_params, 
                            osc_channels=["numu_survival", "antinumu_survival", "nue_appearance", "antinue_appearance"])
 
-  fig = plt.figure(figsize=(8, 5))
-    
-  axl = fig.add_axes((0.1,0.3,0.39,0.65))
-  numu_surv_prob_l, = axl.plot(Es, numu_surv_prob)
-  antinumu_surv_prob_l, = axl.plot(Es, antinumu_surv_prob, ls="dashed")
-  axl.set_xlabel(r"$E_{\nu}$ [GeV]", size="x-large")
-  axl.set_ylabel(r"$P_{\nu_\mu \rightarrow \nu_\mu}$", size="x-large")
-  axl.set_ylim([0,1])
-  
-  axr = fig.add_axes((0.6,0.3,0.39,0.65))
-  nue_surv_prob_l, = axr.plot(Es, nue_app_prob, label=r"neutrino")
-  antinue_surv_prob_l, = axr.plot(Es, antinue_app_prob, ls="dashed", label=r"antineutrino")
-  axr.set_xlabel(r"$E_{\nu}$ [GeV]", size="x-large")
-  axr.set_ylabel(r"$P_{\nu_\mu \rightarrow \nu_\mathrm{e}}$", size="x-large")
-  axr.set_ylim([0,0.15])
-  axr.legend()
+  InteractiveOscProbPlot_fig = plt.figure(figsize=(10, 6))
 
-  axdmsq31 = fig.add_axes((0.25, 0.1, 0.5, 0.03))
+  alpha = 0.2
+
+  def norm(x, loc, scale):
+    return (1.0/(np.sqrt(2*np.pi)*scale)) * np.exp(-np.power(x-loc,2)/np.power(scale,2))
+  
+  nu_unosc_evr = norm(Es, loc=Energy_Distribution_GeV["peak"], scale=Energy_Distribution_GeV["width"]) \
+                   /norm(Energy_Distribution_GeV["peak"], loc=Energy_Distribution_GeV["peak"], scale=Energy_Distribution_GeV["width"])
+  antinu_unosc_evr = nu_unosc_evr*0.3
+
+  axpl = InteractiveOscProbPlot_fig.add_axes((0.1,0.725,0.39,0.26))
+  numu_unosc_evr_lo_l = axpl.fill_between(Es, nu_unosc_evr*0.9, np.zeros_like(Es), fc="grey", alpha=0.1)
+  numu_surv_prob_lo, = axpl.plot(Es, numu_surv_prob, c=colors["numu"], alpha=alpha)
+  numu_surv_prob_l, = axpl.plot(Es, numu_surv_prob, c=colors["numu"], label=r"muon neutrino")
+  antinumu_surv_prob_lo, = axpl.plot(Es, antinumu_surv_prob, c=colors["antinumu"], alpha=alpha, ls="dashed")
+  antinumu_surv_prob_l, = axpl.plot(Es, antinumu_surv_prob, c=colors["antinumu"], ls="dashed", label=r"muon antineutrino")
+  axpl.set_xlabel(r"$E_{\nu}$ [GeV]", size="large")
+  axpl.set_ylabel(r"$P_{\nu_\mu \rightarrow \nu_\mu}$", size="x-large")
+  axpl.set_ylim([0,1.1])
+  axpl.legend()
+  
+  axpr = InteractiveOscProbPlot_fig.add_axes((0.6,0.725,0.39,0.26))
+  numu_unosc_evr_lo_r = axpr.fill_between(Es, nu_unosc_evr*0.3*0.9, np.zeros_like(Es), fc="grey", alpha=0.1)
+  nue_surv_prob_lo, = axpr.plot(Es, nue_app_prob, c=colors["nue"], alpha=alpha)
+  nue_surv_prob_l, = axpr.plot(Es, nue_app_prob, c=colors["nue"], label=r"electron neutrino")
+  antinue_surv_prob_lo, = axpr.plot(Es, antinue_app_prob, c=colors["antinue"], alpha=alpha, ls="dashed")
+  antinue_surv_prob_l, = axpr.plot(Es, antinue_app_prob, c=colors["antinue"], ls="dashed", label=r"electron antineutrino")
+  axpr.set_xlabel(r"$E_{\nu}$ [GeV]", size="large")
+  axpr.set_ylabel(r"$P_{\nu_\mu \rightarrow \nu_\mathrm{e}}$", size="large")
+  axpr.set_ylim([0,0.3])
+  axpr.legend()
+
+  axel = InteractiveOscProbPlot_fig.add_axes((0.1,0.05,0.39,0.26))
+  numu_surv_evr_lo, = axel.plot(Es, numu_surv_prob*nu_unosc_evr, c=colors["numu"], alpha=alpha)
+  numu_surv_evr_l, = axel.plot(Es, numu_surv_prob*nu_unosc_evr, c=colors["numu"], label=r"muon neutrino")
+  nue_app_evr_lo, = axel.plot(Es, nue_app_prob*nu_unosc_evr, c=colors["nue"], alpha=alpha)
+  nue_app_evr_l, = axel.plot(Es, nue_app_prob*nu_unosc_evr, c=colors["nue"], label=r"electron neutrino")
+  axel.set_xlabel(r"$E_{\nu}$ [GeV]", size="large")
+  axel.set_ylabel(r"Observed Neutrinos")
+  axel_ymax = np.max(numu_surv_prob*nu_unosc_evr)*1.1
+  axel.set_ylim([0, axel_ymax])
+  axel.legend()
+
+  axer = InteractiveOscProbPlot_fig.add_axes((0.6,0.05,0.39,0.26))
+  antinumu_surv_evr_lo, = axer.plot(Es, antinumu_surv_prob*antinu_unosc_evr, c=colors["antinumu"], alpha=alpha, ls="dashed")
+  antinumu_surv_evr_l, = axer.plot(Es, antinumu_surv_prob*antinu_unosc_evr, c=colors["antinumu"], ls="dashed", label=r"muon antineutrino")
+  antinue_app_evr_lo, = axer.plot(Es, antinue_app_prob*antinu_unosc_evr, c=colors["antinue"], alpha=alpha, ls="dashed")
+  antinue_app_evr_l, = axer.plot(Es, antinue_app_prob*antinu_unosc_evr, c=colors["antinue"], ls="dashed", label=r"electron antineutrino")
+  axer.set_xlabel(r"$E_{\nu}$ [GeV]", size="large")
+  axer.set_ylabel(r"Observed Antineutrinos")
+  axer.set_ylim([0, axel_ymax])
+  axer.legend()
+
+  axdmsq31 = InteractiveOscProbPlot_fig.add_axes((0.25, 0.55, 0.5, 0.03))
   dmsq31_slider = Slider(
       ax=axdmsq31,
       label=r"$\Delta\mathrm{m}_{31}^{2}$ [$10^{-3}$ eV]",
@@ -341,8 +399,35 @@ def InteractiveOscProbPlot(Es, osc_params):
       valmax=2.7,
       valinit=osc_params["Dmsq31"]*1E3,
   )
+
+  axdmsq21 = InteractiveOscProbPlot_fig.add_axes((0.25, 0.51, 0.5, 0.03))
+  dmsq21_slider = Slider(
+      ax=axdmsq21,
+      label=r"$\Delta\mathrm{m}_{21}^{2}$ [$10^{-5}$ eV]",
+      valmin=6,
+      valmax=9,
+      valinit=osc_params["Dmsq21"]*1E5,
+  )
   
-  axs23sq = fig.add_axes((0.25, 0.05, 0.5, 0.03))
+  axs12sq = InteractiveOscProbPlot_fig.add_axes((0.25, 0.47, 0.5, 0.03))
+  s12sq_slider = Slider(
+      ax=axs12sq,
+      label=r"$\mathrm{sin}^{2}(\theta_{12})$",
+      valmin=0.25,
+      valmax=0.36,
+      valinit=osc_params["s12sq"],
+  )
+
+  axs13sq = InteractiveOscProbPlot_fig.add_axes((0.25, 0.43, 0.5, 0.03))
+  s13sq_slider = Slider(
+      ax=axs13sq,
+      label=r"$\mathrm{sin}^{2}(\theta_{13})$",
+      valmin=0.015,
+      valmax=0.025,
+      valinit=osc_params["s13sq"],
+  )
+  
+  axs23sq = InteractiveOscProbPlot_fig.add_axes((0.25, 0.39, 0.5, 0.03))
   s23sq_slider = Slider(
       ax=axs23sq,
       label=r"$\mathrm{sin}^{2}(\theta_{23})$",
@@ -351,7 +436,7 @@ def InteractiveOscProbPlot(Es, osc_params):
       valinit=osc_params["s23sq"],
   )
   
-  axdelta = fig.add_axes((0.25, 0, 0.5, 0.03))
+  axdelta = InteractiveOscProbPlot_fig.add_axes((0.25, 0.35, 0.5, 0.03))
   delta_slider = Slider(
       ax=axdelta,
       label=r"$\delta_\mathrm{CP}/\pi$",
@@ -363,6 +448,9 @@ def InteractiveOscProbPlot(Es, osc_params):
   # The function to be called anytime a slider's value changes
   def update(val):
       var_params["Dmsq31"] = dmsq31_slider.val * 1E-3
+      var_params["Dmsq21"] = dmsq21_slider.val * 1E-5
+      var_params["s12sq"] = s12sq_slider.val
+      var_params["s13sq"] = s13sq_slider.val
       var_params["s23sq"] = s23sq_slider.val
       var_params["delta"] = delta_slider.val * np.pi
       
@@ -374,13 +462,19 @@ def InteractiveOscProbPlot(Es, osc_params):
       antinumu_surv_prob_l.set_ydata(antinumu_surv_prob)
       nue_surv_prob_l.set_ydata(nue_app_prob)
       antinue_surv_prob_l.set_ydata(antinue_app_prob)
+
+      numu_surv_evr_l.set_ydata(numu_surv_prob*nu_unosc_evr)
+      nue_app_evr_l.set_ydata(nue_app_prob*nu_unosc_evr)
+      antinumu_surv_evr_l.set_ydata(antinumu_surv_prob*antinu_unosc_evr)
+      antinue_app_evr_l.set_ydata(antinue_app_prob*antinu_unosc_evr)
       
-      fig.canvas.draw_idle()
+      InteractiveOscProbPlot_fig.canvas.draw_idle()
   
   
   # register the update function with each slider
   dmsq31_slider.on_changed(update)
+  dmsq21_slider.on_changed(update)
+  s12sq_slider.on_changed(update)
+  s13sq_slider.on_changed(update)
   s23sq_slider.on_changed(update)
   delta_slider.on_changed(update)
-
-  return fig
